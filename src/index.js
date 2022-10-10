4143ff5fbb4ea131707590d2d6edc2486afbf705
Klinton Ponari <73606015+KlintiPonari@users.noreply.github.com>
2022-10-11 01:37:15 +0200

    Headers update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h id="all-notes">All notes</h>
+    <h id="new-note">My note<br></br>Category &#9679; Date</h>
     <App />
   </React.StrictMode>
 );
